6ba700279ee883eb0dc99a9d0a1d11fdd712c6e5
Anik <anik.khondokar09@gmail.com>
2025-06-13 15:07:02 +0600

    Exit program

--- Java_Lecture_Homeworks/Practice/src/Exit_Program.java
+++ Java_Lecture_Homeworks/Practice/src/Exit_Program.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Exit_Program {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        a=50; b=0;
+        if(b==0){
+            System.err.println("There is an error!");
+            System.exit(1);
+        }
+        int result=a/b;
+        System.out.println(result);
+    }
 }
